5a52dce08f19dd93b65b5357ae0983846e45bef0
Darknez07 <rishikakkar42@gmail.com>
2021-11-13 19:42:03 +0530

    New model

---
+++ Web_Model/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template, request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def router():
+    if request.method == 'GET':
+        return render_template('index.html', answer=False)
+    else:
+        try:
+            val = int(request.form['dis'])
+        except:
+            val = 0
+        print(val)
+        return render_template('index.html', answer=True,num=val)
+
+@app.route('/submit',methods=['POST','GET'])
+def subs():
+    return "<h1> Hey </h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
